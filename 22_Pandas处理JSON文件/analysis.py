e18e1362875e924330a8d6e2726ef15ea7e0f7ea
jacky <profit_littlec@163.com>
2019-09-01 11:52:50 +0800

    完善挑战代码

--- 22_Pandas处理JSON文件/analysis.py
+++ 22_Pandas处理JSON文件/analysis.py
@@ -1,21 +1,7 @@
-#!/usr/bin/env python3
-# _*_ coding:utf-8 _*_
-
-import json
-import pandas as pds
-
+import pandas 
 
 def analysis(file, user_id):
-    times = 0
-    minutes = 0
-
-    if user_id and file:
-        with open(file) as f:
-            data = pds.read_json(f)
-            targets = data[data['user_id'] == user_id]
-            times = targets.size
-            minutes = targets['minutes'].sum()
-    return times, minutes
-
-if __name__ == '__main__':
-    print(analysis('./user_study.json', 164830))
+    data = pandas.read_json(file)
+    targets = data[data['user_id'] == user_id].minutes
+    targets = data[data.user_id == user_id].minutes
+    return targets.count(), targets.sum()
