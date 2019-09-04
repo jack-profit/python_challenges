6f3f4b1ebe17091130dae108da41d64daa1b31e7
jacky <profit_littlec@163.com>
2019-09-04 11:26:58 +0800

    完善代码并通过挑战

--- 24_时间序列分析挑战/apple_analysis.py
+++ 24_时间序列分析挑战/apple_analysis.py
@@ -1,18 +1,14 @@
 # _*_ coding:utf-8 _*_
 import pandas as pd
+from pandas import offsets, Series
 
 def quarter_volume():
     data = pd.read_csv('apple.csv', header=0)
 
-    '''
-    1.使用pandas选择数据
-    2.转换为时间序列
-    3.按季度重采样并排序
-
-    目标：返回交易总量降序排名第2的季度的成交总量 sum()
-    '''
-
-    return second_volume
+    target = pd.Series(list(data.Volume), index=data.Date) # 筛选数据
+    target.index = pd.to_datetime(target.index) # 转换时间序列
+    target = target.resample('Q').sum().sort_values() # 重采样并排序
+    return target[-2]
 
 if __name__ == '__main__':
-    quarter_volume()
+    print(quarter_volume())
